b8a17188f75d2e90a4169c0e8622f2aa097f3fb5
Antonio Mauricio <antonioimportant@gmail.com>
2025-04-23 15:52:23 -0300

    feat: adds jsonFile validation layer

--- GoogleFormsJson.app/Program.cs
+++ GoogleFormsJson.app/Program.cs
@@ -31,6 +31,27 @@ internal class Program
 
         if (form == null) throw new Exception("failed to parse form JSON file");
 
+        var errors = new List<string>();
+
+        if (string.IsNullOrEmpty(form.FormTitle)) errors.Add("formTitle cannot be empty");
+
+        if (form.Questions.Count == 0) errors.Add("form.questions should have at least 1 question");
+
+        for (var i = 0; i < form.Questions.Count; i++)
+        {
+            if (string.IsNullOrEmpty(form.Questions[i].Title)) errors.Add($"[question at index {i}] question.title cannot be null");
+            if (form.Questions[i].QuestionType.Equals(null)) errors.Add($"[question at index {i}] question.type cannot be null");
+            if (string.IsNullOrEmpty(form.Questions[i].CorrectOption)) errors.Add($"[question at index {i}] question.correctAsnwer cannot be empty");
+            if (form.Questions[i].Options.Count <= 1) errors.Add($"[question at index {i}] a question should have at least 2 options");
+
+            for (var j = 0; j < form.Questions[i].Options.Count; j++)
+            {
+                if (string.IsNullOrEmpty(form.Questions[i].Options[j])) errors.Add($"[question at index {i}] option {j} cannot be empty");
+            }
+        }
+
+        if (errors.Count > 0) throw new Exception($"found validation errors: {string.Join(", ", errors.ToArray())}");
+
         return form;
     }
 }
